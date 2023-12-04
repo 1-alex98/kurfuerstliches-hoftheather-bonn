dbcfa23a4332a164524e382ef1e57ad1032b4af9
alex <alexander.von.trostorff@gmail.com>
2023-12-04 14:46:39 +0100

    Small fixes

--- load.js
+++ load.js
@@ -1,16 +1,14 @@
 const response = fetch('/layout.html').then(function (response) {
     return response.text();
 })
-function changeColor() {
-    let nav = document.getElementById("fixed-nav"); // nav is a fixed element
 
-    // Get the element behind the nav
-    let rect = nav.getBoundingClientRect();
+function setColourBasedOnBack(element) {
+    let rect = element.getBoundingClientRect();
     let elementsBehindNav = document.elementsFromPoint(rect.left + 1, rect.top + 1);
     elementsBehindNav.pop(); //topmost is not interesting it is the nav itself
     // Check if the element or its parents have the CSS class "light-navigation"
     let isLightNavigation = false;
-    for(let element of elementsBehindNav) {
+    for (let element of elementsBehindNav) {
         while (element) {
             if (element.classList && element.classList.contains("light-navigation")) {
                 isLightNavigation = true;
@@ -22,14 +20,31 @@ function changeColor() {
 
     // Set nav style class based on the condition
     if (isLightNavigation) {
-        nav.classList.add("light-nav");
+        element.classList.add("light-nav");
     } else {
-        nav.classList.remove("light-nav");
+        element.classList.remove("light-nav");
     }
+}
+
+let mybutton;
+let nav;
+function changeColor() {
+    if(!mybutton) {
+        mybutton = document.getElementById("topBtn");
+    }
+    if(!nav) {
+        nav = document.getElementById("fixed-nav");
+    }
+
+    // Get the element behind the nav
+    setColourBasedOnBack(mybutton);
+    setColourBasedOnBack(nav);
+    topButtonScroll()
 }
 
 window.onload = async function () {
     let content = document.getElementById("content").innerHTML;
+
     document.getElementsByTagName('body')[0].innerHTML = await response;
     document.getElementById("content").innerHTML = content;
     changeColor();
@@ -37,3 +52,21 @@ window.onload = async function () {
 
 
 window.onscroll = changeColor
+
+
+// Get the button:
+
+
+function topButtonScroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
